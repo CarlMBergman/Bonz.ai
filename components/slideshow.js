6bbde77fef16af1d502e335c87eb102ce1b47750
CarlMBergman <113527185+CarlMBergman@users.noreply.github.com>
2023-02-20 13:53:22 +0100

    mycket

--- components/slideshow.js
+++ components/slideshow.js
@@ -12,7 +12,7 @@ function currentSlide(n) {
 function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("slideshow__slides");
-  let dots = document.getElementsByClassName("slideshow__dots--dot");
+  let dots = document.getElementsByClassName("slideshow__dot");
   if (n > slides.length) {slideIndex = 1}    
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
